52c4c985c505fba0341173c73437a6a26d4d1503
keikamiguchi <kei.kamiguchi517@gmail.com>
2021-01-13 15:56:50 +0900

    データ型変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_151127) do
+ActiveRecord::Schema.define(version: 2021_01_13_065535) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "status"
-    t.datetime "deadline"
+    t.date "deadline"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
